4f2f1a25460c9fb98e9994dee1166a7828a8c7e9
AgU131 <agu.heredia.urbinatti@gmail.com>
2025-05-16 16:13:02 -0300

    chamber

---
+++ chamber/scripts/date.js
@@ -0,0 +1,6 @@
+// Dates and CopyWrite
+let year = new Date().getFullYear();
+let lastModified = document.lastModified;
+// \u00A9 --> the unicode for the copywrite symbol. "&copy;" in html 
+document.getElementById("copywrite").innerHTML = '&copy;' + ` ${year}  Timbuktu Chamber of Commerce`;
+document.getElementById("lastModified").textContent = `Last Modification: ${lastModified}`;	  
