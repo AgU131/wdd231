ffea45490a9549af6e87add02fe9032ff7029606
AgU131 <agu.heredia.urbinatti@gmail.com>
2025-05-19 18:11:50 -0300

    js fixed done

--- scripts/weather.js
+++ scripts/weather.js
@@ -3,7 +3,11 @@ const currentTemp = document.querySelector('#current-temp');
 const weatherIcon = document.querySelector('#weather-icon');
 const captionDesc = document.querySelector('figcaption');
 
-const url = 'https://api.openweathermap.org/data/2.5/weather?lat=49.75&lon=6.64&units=metric&appid=400077a7d200ba0bfc960dbb8c5908f4';
+const key = "400077a7d200ba0bfc960dbb8c5908f4"
+const lat = "49.75"
+const long = "6.64"
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}&units=metric`;
 
 
 async function apiFetch() {
@@ -12,7 +16,7 @@ async function apiFetch() {
     if (response.ok) {
       const data = await response.json();
       console.log(data); // testing only
-      // displayResults(data); // uncomment when ready
+      displayResults(data); // uncomment when ready
     } else {
         throw Error(await response.text());
     }
@@ -24,7 +28,7 @@ async function apiFetch() {
 apiFetch();
 
 function displayResults(data) {
-  currentTemp.innerHTML = `${data.main.temp}&deg;F`;
+  currentTemp.innerHTML = `${data.main.temp}&deg;C`;
   const iconsrc = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
   let desc = data.weather[0].description;
   weatherIcon.setAttribute('src', iconsrc);
