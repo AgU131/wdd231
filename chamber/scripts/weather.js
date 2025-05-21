f553008ff841f54da849f8a001638ecf4638cdb4
AgU131 <agu.heredia.urbinatti@gmail.com>
2025-05-21 18:53:13 -0300

    error fixed: mispelled url variable

--- chamber/scripts/weather.js
+++ chamber/scripts/weather.js
@@ -6,11 +6,11 @@ const forecast = document.querySelector('#forecast');
 // const captionDesc = document.querySelector('figcaption');
 
 const key = "dd13352d8628298ab841d7e562984852"
-const lat = "-31.41967449183622"
-const long = "-64.18860315826853"
+const lat = "-31.42"
+const long = "-64.19"
 
-const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}&units=metric`;
-const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${key}&units=metric`;
+// const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${key}&units=metric`;
+const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=${key}&units=metric`;
 
 
 async function apiFetch() {
@@ -29,6 +29,26 @@ async function apiFetch() {
 }
 
 apiFetch();
+
+
+// convert a uniix time stamp to normal time
+  function unixtime(x) {
+    // multiplied by 1000 so that the argument is in milliseconds, not seconds
+    const date = new Date(x * 1000);
+
+    // Hours part from the timestamp
+    let hours = date.getHours();
+    if (hours>12) {hours=hours-12}
+
+    // Minutes part from the timestamp
+    const minutes = "0" + date.getMinutes();
+
+    // Will display time in 10:30:23 format
+    const formattedTime = `${hours}:${minutes.substring(minutes.length - 2)}`;
+    return formattedTime
+  
+}
+
 
 function displayResults(data) {
   const iconsrc = `https://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`;
@@ -52,40 +72,40 @@ function displayResults(data) {
   report.appendChild(description)
   
   const high = document.createElement('p')
-  high.innerHTML = `High: ${data.list[0].main.temp_min}&deg;C`
+  high.innerHTML = `High: ${data.list[0].main.temp_max}&deg;C`
   report.appendChild(high)
   
   const low = document.createElement('p')
-  low.innerHTML = `Low: ${data.list[0].main.temp_max}&deg;C`
-  report.appendChild(description)
+  low.innerHTML = `Low: ${data.list[0].main.temp_min}&deg;C`
+  report.appendChild(low)
   
   const humidity = document.createElement('p')
   humidity.innerHTML = `Humidity: ${data.list[0].main.humidity}%`
   report.appendChild(humidity)
   
   const sunrise = document.createElement('p')
-  sunrise.innerHTML = `Sunrise: ${data.city.sunrise}am`
+  sunrise.innerHTML = `Sunrise: ${unixtime(data.city.sunrise)}am`
   report.appendChild(sunrise)
   
   const sunset = document.createElement('p')
-  sunset.innerHTML = `Sunset: ${data.city.sunrise}pm`
+  sunset.innerHTML = `Sunset: ${unixtime(data.city.sunset)}pm`
   report.appendChild(sunset)
 
 
-//forecast
+  //forecast
   const dayNames=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
   const d = new Date();
   
-  const temp1=document.createElement('p')
-  temp1.innerHTML = `${dayNames[d.getDay()]}: ${data.list[0].main.temp}&deg;F`
+  const temp1 = document.createElement('p')
+  temp1.innerHTML = `${dayNames[d.getDay()]}: ${data.list[0].main.temp}&deg;C`
   forecast.appendChild(temp1)
 
 
-  const temp2=document.createElement('p')
-  temp2.innerHTML = `${dayNames[d.getDay()+1]}: ${data.list[8].main.temp}&deg;F`
+  const temp2 = document.createElement('p')
+  temp2.innerHTML = `${dayNames[d.getDay()+1]}: ${data.list[8].main.temp}&deg;C`
   forecast.appendChild(temp2)
 
-  const temp3=document.createElement('p')
-  temp3.innerHTML = `${dayNames[d.getDay()+2]}: ${data.list[16].main.temp}&deg;F`
+  const temp3 = document.createElement('p')
+  temp3.innerHTML = `${dayNames[d.getDay()+2]}: ${data.list[16].main.temp}&deg;C`
   forecast.appendChild(temp3)
 }
