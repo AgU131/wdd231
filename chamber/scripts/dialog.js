1bc222c4e3dfb3ce33032418a905daf35e92006f
AgU131 <agu.heredia.urbinatti@gmail.com>
2025-05-27 18:38:55 -0300

    thanks page done

---
+++ chamber/scripts/dialog.js
@@ -0,0 +1,20 @@
+// WEEK 4 ACTIVITY DIALOG
+const courseDetails = document.querySelector("#course-details")
+// const closeModal = document.querySelector("#closeModal");
+const courseDiv = document.querySelector("#coursesList div"); 
+
+function displayCourseDetails(course) {
+  courseDetails.innerHTML = '';
+  courseDetails.innerHTML = `
+    <button id="closeModal">X</button>
+    <h2>${course.subject} ${course.number}</h2>
+    <h3>${course.title}</h3>
+    <p><strong>Credits</strong>: ${course.credits}</p>
+    <p><strong>Certificate</strong>: ${course.certificate}</p>
+    <p>${course.description}</p>
+    <p><strong>Technologies</strong>: ${course.technology.join(', ')}</p>
+  `;
+  courseDetails.showModal();
+
+  closeModal.addEventListener("click", () => courseDetails.close())
+}
