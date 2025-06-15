6dc5b886b624f56b5a03408b9d0f2cfa4ce84da4
AgU131 <agu.heredia.urbinatti@gmail.com>
2025-06-14 22:42:52 -0300

    sliders added

--- final/scripts/home_page.js
+++ final/scripts/home_page.js
@@ -1,17 +1,17 @@
 const slider = document.querySelector('.slider');
-const images = document.querySelectorAll('.slider img');
+const image = document.querySelectorAll('.slider img');
 const containerWidth = document.querySelector('.slider-container').offsetWidth;
 let animationDuration = 20; // Duración inicial de la animación
 
 // Ajusta la duración de la animación basada en el número de imágenes
-animationDuration = (images.length / 2) * 5; // Ajusta el 5 según tus necesidades
+animationDuration = (image.length / 2) * 5; // Ajusta el 5 según tus necesidades
 slider.style.animationDuration = `${animationDuration}s`;
 
 // Duplica las imágenes si es necesario para asegurar un bucle continuo
 if (slider.scrollWidth <= containerWidth) {
     const numCopies = Math.ceil(containerWidth / slider.scrollWidth) + 1; // Calcula cuantas copias necesita
     for (let i = 0; i < numCopies; i++) {
-        images.forEach(img => {
+        image.forEach(img => {
         const clone = img.cloneNode(true);
         slider.appendChild(clone);
         });
@@ -19,24 +19,24 @@ if (slider.scrollWidth <= containerWidth) {
 }
 
 
-// //HERO Images Slider
-// let currentImageIndex = 0;
-// const images = document.querySelectorAll('#sliderImage'); // Selecciona todas las imágenes
-// const totalImages = images.length;
+//HERO Images Slider
+let currentImageIndex = 0;
+const images = document.querySelectorAll('#sliderImage'); // Selecciona todas las imágenes
+const totalImages = images.length;
 
-// function showImage(index) {
-//     // Oculta todas las imágenes
-//     images.forEach(img => img.classList.remove('active'));
+function showImage(index) {
+    // Oculta todas las imágenes
+    images.forEach(img => img.classList.remove('active'));
 
-//     // Muestra la imagen en el índice dado
-//     images[index].classList.add('active');
-// }
+    // Muestra la imagen en el índice dado
+    images[index].classList.add('active');
+}
 
-// function changeImage(direction) {
-//     currentImageIndex = (currentImageIndex + direction + totalImages) % totalImages;
-//     showImage(currentImageIndex);
-// }
+function changeImage(direction) {
+    currentImageIndex = (currentImageIndex + direction + totalImages) % totalImages;
+    showImage(currentImageIndex);
+}
 
-// // Mostrar la primera imagen al cargar la página
-// showImage(currentImageIndex);
+// Mostrar la primera imagen al cargar la página
+showImage(currentImageIndex);
 
