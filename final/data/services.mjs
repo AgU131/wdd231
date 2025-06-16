e3188c1f07c723944132d2ee2d7c66d17fbd7702
AgU131 <agu.heredia.urbinatti@gmail.com>
2025-06-16 19:38:11 -0300

    index almost ready (just js random missing) and dialog added

---
+++ final/data/services.mjs
@@ -0,0 +1,19 @@
+export const requirements = [
+  {
+    "title": "Bambulab A1 Mini",
+    "description": "The Bambulab A1 Mini is a compact and efficient 3D printer designed for both beginners and experienced users. It features a user-friendly interface, high precision printing, and a variety of compatible filaments.",
+    "especifications": {
+      "build_volume": "150 x 150 x 150 mm",
+      "layer_height": "0.1 - 0.4 mm",
+      "nozzle_diameter": "0.4 mm",
+      "filament_types": ["PLA", "ABS", "PETG"],
+      "connectivity": ["USB", "Wi-Fi"],
+      "weight": "5 kg",
+      "filaments_slots": 1
+    },
+    "cost": 'Send us your 3D Design!'
+  },
+  {
+
+  }
+]
---
+++ final/scripts/aboutservice.js
@@ -0,0 +1,51 @@
+// import { services } from '../data/services.mjs'
+
+// Modal General Constants
+const serviceModal = document.querySelector("#serviceModal")
+const closeModal = document.querySelector("#closeModal");
+const modalTitle = document.querySelector("#modalTitle");
+const modalDetails = document.querySelector("#modalDetails");
+
+// Modal each Service Button Constants
+const printBtn = document.querySelector('#serviceBtnPrint');
+const modelBtn = document.querySelector('#serviceBtnModel');
+const fixBtn = document.querySelector('#serviceBtnFix');
+const consultBtn = document.querySelector('#serviceBtnConsult');
+
+// Function to close the modal
+closeModal.addEventListener("click", () => serviceModal.close())
+
+// Generic function for all Modals
+function configServiceModalInfo(button, title, description, cost) {
+  button.addEventListener('click', () => {
+    modalTitle.innerHTML = title
+    modalDetails.innerHTML = `
+    <p>Description:</p>
+    <ul>
+       ${description.map(desc => `<li>"${desc}"</li>`).join('')}
+    </ul>
+    <p>COST: ${cost}</p>
+    `;
+    serviceModal.showModal();
+  });
+}
+
+
+// Calling Generic function for each Modal with each service info
+configServiceModalInfo(
+  printBtn,
+  '3D Printing',
+  [
+    'Discount tickets to all Bowl Games.',
+    "You'll get a nice plaque with the mayor's stamp on it.",
+    'You get to ride on a float Saint Patty\'s Day.',
+    'Monthly newsletter with local business updates.',
+    'Early access to community yard sales.'
+  ],
+  'Send us your 3D Design!'
+);
+
+
+
+
+//HIDDEN DATE for the Form
